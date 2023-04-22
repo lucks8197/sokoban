ad6693666b2ecb5bd00b1aa0c69de6777237253d
lucas <119272914+lucks8197@users.noreply.github.com>
2023-04-22 18:45:41 -0500

    c
    images work!!!! at least in this image testing class, still need to add it to main thing

--- ImageTesting.java
+++ ImageTesting.java
@@ -1,12 +1,23 @@
-import javax.swing.JFrame;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.*;
+import java.awt.Color;
+import java.awt.RenderingHints;
+import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
 
-public class ImageTesting {
+public class ImageTesting extends Scene {
+
+   static BufferedImage picture;
+   
    public static void main(String[] args) {
       JFrame f = new JFrame("Image Testing");
       f.resize(new Dimension(500,500));
-      Scene sc = new ImageScene();
-      // f.addMouseListener((MouseListener) sc);
+      Scene sc = new ImageTesting();
       f.add(sc);
       if (true) {
          f.setExtendedState(JFrame.MAXIMIZED_BOTH);
@@ -16,17 +27,20 @@ public class ImageTesting {
          device.setFullScreenWindow(f); 
       }
       f.setVisible(true);
+      
+      try {picture=ImageIO.read(new File("square.png"));} catch (Exception FileNotFoundException) {System.out.println("PP");}
+      
       while (true) {
          sc.getScreenResizeInfo(f);
          f.repaint();  
       }
    }
    
-   public void simg(Graphics2D g2d, double x, double y, double xs, double ys) {
+   public void simg(Graphics2D g2d, BufferedImage img, double x, double y, double xs, double ys) {
       // kind of like a lerp
-      if (ww>wh && wh*screenRatio <= ww) g2d.fill(new Rectangle2D.Double(
+      if (ww>wh && wh*screenRatio <= ww) imageSuffer(g2d, img,
             ((x/(100*screenRatio))*w)+lw, ((y/100)*h)+lh, (xs/100)*(w/screenRatio), (ys/100)*(h)
-            ));
+            );
       else g2d.fill(new Rectangle2D.Double(
             ((x/(100*screenRatio))*w)+lw, ((y/100)*h)+lh, (xs/100)*(w/screenRatio), (ys/100)*(h)
             ));
@@ -42,11 +56,20 @@ public class ImageTesting {
       rect2(g2d, x-xs, y-ys, x+xs, y+ys); 
    }
    
-
+   static double xs = 100;
+   @Override
+   protected void paintComponent(Graphics g) {
+      Graphics2D g2d = (Graphics2D)g;
+      g2d.setColor(new Color(255,255,0));
+      
+      srect(g2d,0,0,50,50);simg(g2d,picture,0,0,50,50);
+   }   
    
-   
-}
-
-class ImageScene extends Scene {
-
+   public void imageSuffer(Graphics2D g2d, BufferedImage img, double x, double y, double xsize, double ysize) {
+      AffineTransform at = new AffineTransform();
+      at.scale((double)xsize/(img.getWidth()),(double)ysize/(img.getHeight()));
+      at.translate(x,y);
+      BufferedImageOp bio = (BufferedImageOp)new AffineTransformOp(at, g2d.getRenderingHints());
+      g2d.drawImage(img,bio,0,0);      
+   }   
 }
